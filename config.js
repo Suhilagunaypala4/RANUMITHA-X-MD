85950ce1a2f0ba3cd91adcb9e6a35e7e2a21b4e1
Suhilagunaypala4 <gunathilakasuhila@gmail.com>
2025-08-08 22:48:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "1ysimbaJ#iPPIHFABxJsa1dPa3otymPMt4lXXhq0Pllg2AcHpHzc",   // add your Session Id 
+  SESSION_ID: process.env.SESSION_ID || "pqFS3bhL#ey8VX9Qec5V6adaB0XBZoPU1XUc9VBiyjxvcE4jhGDI",   // add your Session Id 
   OWNER_NUM: process.env.OWNER_NUM || "94762095304",
   DEV: process.env.DEV || "94762095304",
   PREFIX: process.env.PREFIX || ".",
